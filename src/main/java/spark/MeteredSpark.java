fe7473e8065202bea61a913e291f9fe2fd1d7076
Ilya Murzinov <murz42@gmail.com>
2019-01-19 10:40:56 +0100

    refactoring

---
+++ src/main/java/spark/MeteredSpark.java
@@ -0,0 +1,69 @@
+package spark;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import spark.route.HttpMethod;
+
+import java.util.concurrent.Callable;
+
+public class MeteredSpark extends Routable {
+
+    static final String METRIC_NAME = "http.server.requests";
+    static final String URL_TAG = "url";
+    static final String METHOD_TAG = "method";
+
+    private final Service service;
+    private final MeterRegistry registry;
+
+    private MeteredSpark(MeterRegistry registry) {
+        this.registry = registry;
+        this.service = Service.ignite();
+    }
+
+    public static MeteredSpark meteredService(MeterRegistry registry) {
+        return new MeteredSpark(registry);
+    }
+
+    @Override
+    protected void addRoute(HttpMethod httpMethod, RouteImpl route) {
+        addRoute(httpMethod.name(), route);
+    }
+
+    @Override
+    protected void addRoute(String httpMethod, RouteImpl route) {
+        service.addRoute(httpMethod, new RouteImpl(route.getPath()) {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                return metered(route.getPath(), httpMethod, () -> route.handle(request, response));
+            }
+        });
+    }
+
+    @Override
+    protected void addFilter(HttpMethod httpMethod, FilterImpl filter) {
+        service.addFilter(httpMethod, filter);
+    }
+
+    @Override
+    protected void addFilter(String httpMethod, FilterImpl filter) {
+        service.addFilter(httpMethod, filter);
+    }
+
+    public void path(String route, RouteGroup routeGroup) {
+        service.path(route, routeGroup);
+    }
+
+    public void awaitInitialization() {
+        service.awaitInitialization();
+    }
+
+    public void stop() {
+        service.stop();
+        service.awaitStop();
+    }
+
+    private Object metered(String path, String method, Callable<Object> s) throws Exception {
+        final Timer timer = registry.timer(METRIC_NAME, URL_TAG, path, METHOD_TAG, method);
+        return timer.recordCallable(s);
+    }
+}
--- src/test/java/spark/SparkMeteredFunctionalTest.java
+++ src/test/java/spark/SparkMeteredFunctionalTest.java
@@ -11,31 +11,41 @@ import static io.restassured.RestAssured.given;
 import static java.lang.Runtime.getRuntime;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
-import static spark.SparkMetered.METHOD_TAG;
-import static spark.SparkMetered.METRIC_NAME;
-import static spark.SparkMetered.URL_TAG;
+import static spark.MeteredSpark.METHOD_TAG;
+import static spark.MeteredSpark.METRIC_NAME;
+import static spark.MeteredSpark.URL_TAG;
+import static spark.MeteredSpark.meteredService;
 
 class SparkMeteredFunctionalTest {
 
     private static final MeterRegistry registry = new SimpleMeterRegistry();
+    private static final MeteredSpark spark;
     private static final RequestSpecification client = given().port(4567);
     private static final int SLEEP = 10;
 
     static {
-        SparkMetered.registry(registry);
-        SparkMetered.get("/foo_get", (rq, rs) -> longMethod());
-        SparkMetered.post("/foo_post", (rq, rs) -> longMethod());
-        SparkMetered.put("/foo_put", (rq, rs) -> longMethod());
-        SparkMetered.delete("/foo_delete", (rq, rs) -> longMethod());
+        spark = meteredService(registry);
+        spark.get("/foo_get", (rq, rs) -> longMethod());
+        spark.post("/foo_post", (rq, rs) -> longMethod());
+        spark.put("/foo_put", (rq, rs) -> longMethod());
+        spark.delete("/foo_delete", (rq, rs) -> longMethod());
 
-        getRuntime().addShutdownHook(new Thread(Spark::stop));
-        Spark.awaitInitialization();
+        spark.path("/path", () -> {
+            spark.get("/foo_get", (rq, rs) -> longMethod());
+            spark.post("/foo_post", (rq, rs) -> longMethod());
+            spark.put("/foo_put", (rq, rs) -> longMethod());
+            spark.delete("/foo_delete", (rq, rs) -> longMethod());
+        });
+
+        getRuntime().addShutdownHook(new Thread(spark::stop));
+        spark.awaitInitialization();
     }
 
     @Test
     void should_register_get_method_call() {
         // when
         client.get("/foo_get").body();
+        client.get("/path/foo_get").body();
 
         // then
         final Timer timer = registry.get(METRIC_NAME)
@@ -43,7 +53,7 @@ class SparkMeteredFunctionalTest {
                 .tag(METHOD_TAG, "get")
                 .timer();
 
-        assertThat(timer.count()).isEqualTo(1);
+        assertThat(timer.count()).isEqualTo(2);
         assertThat(timer.max(MILLISECONDS)).isGreaterThan(SLEEP);
     }
 
@@ -51,6 +61,7 @@ class SparkMeteredFunctionalTest {
     void should_register_post_method_call() {
         // when
         client.post("/foo_post").body();
+        client.post("/path/foo_post").body();
 
         // then
         final Timer timer = registry.get(METRIC_NAME)
@@ -58,7 +69,7 @@ class SparkMeteredFunctionalTest {
                 .tag(METHOD_TAG, "post")
                 .timer();
 
-        assertThat(timer.count()).isEqualTo(1);
+        assertThat(timer.count()).isEqualTo(2);
         assertThat(timer.max(MILLISECONDS)).isGreaterThan(SLEEP);
     }
 
@@ -66,6 +77,7 @@ class SparkMeteredFunctionalTest {
     void should_register_put_method_call() {
         // when
         client.put("/foo_put").body();
+        client.put("/path/foo_put").body();
 
         // then
         final Timer timer = registry.get(METRIC_NAME)
@@ -73,7 +85,7 @@ class SparkMeteredFunctionalTest {
                 .tag(METHOD_TAG, "put")
                 .timer();
 
-        assertThat(timer.count()).isEqualTo(1);
+        assertThat(timer.count()).isEqualTo(2);
         assertThat(timer.max(MILLISECONDS)).isGreaterThan(SLEEP);
     }
 
@@ -81,6 +93,7 @@ class SparkMeteredFunctionalTest {
     void should_register_delete_method_call() {
         // when
         client.delete("/foo_delete").body();
+        client.delete("/path/foo_delete").body();
 
         // then
         final Timer timer = registry.get(METRIC_NAME)
@@ -88,7 +101,7 @@ class SparkMeteredFunctionalTest {
                 .tag(METHOD_TAG, "delete")
                 .timer();
 
-        assertThat(timer.count()).isEqualTo(1);
+        assertThat(timer.count()).isEqualTo(2);
         assertThat(timer.max(MILLISECONDS)).isGreaterThan(SLEEP);
     }
 
@@ -99,7 +112,6 @@ class SparkMeteredFunctionalTest {
 
     @AfterAll
     static void shutDown() {
-        Spark.stop();
-        Spark.awaitStop();
+        spark.stop();
     }
 }
